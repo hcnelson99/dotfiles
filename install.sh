2c4cbf10c2b2a8e066a710825b436587519f8f65
Henry Nelson <hcnelson99@gmail.com>
2016-04-16 19:58:17 -0400

    Add .tmux.conf

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+set -eux
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim &&
-ln -s ~/dotfiles/.vimrc ~/.vimrc &&
+ln -s .vimrc ~/.vimrc &&
 vim +PluginInstall +qall
+
+ln -s .tmux.conf ~/.tmux.conf
