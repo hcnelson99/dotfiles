854485669e198cff2dd418d0a58ace5e62dde5c7
Henry Nelson <hcnelson99@gmail.com>
2016-04-25 12:07:09 -0400

    Make install script dumber
    Makes debugging partially failed installs simpler for as long as I'm too lazy to write proper error handling code

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-set -u
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim &&
-ln -s ~/dotfiles/.vimrc ~/.vimrc &&
+set -ux
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim 
+ln -s ~/dotfiles/.vimrc ~/.vimrc 
 vim +PluginInstall +qall
 
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 
-ln -s ~/dotfiles/.antigen.zsh ~/.antigen.zsh &&
-ln -s ~/dotfiles/.zshenv ~/.zshenv &&
+ln -s ~/dotfiles/.antigen.zsh ~/.antigen.zsh 
+ln -s ~/dotfiles/.zshenv ~/.zshenv 
 ln -s ~/dotfiles/.zshrc ~/.zshrc
