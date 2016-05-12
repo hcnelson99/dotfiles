f60ab027d23a37fe8d112a9db0c0954519310d0c
Henry Nelson <hcnelson99@gmail.com>
2016-05-12 09:10:09 -0400

    Add .gitconfig and .gitignore

--- install.sh
+++ install.sh
@@ -9,3 +9,6 @@ ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 git clone https://github.com/tarjoilija/zgen.git ~/.zgen
 ln -s ~/dotfiles/.zshenv ~/.zshenv 
 ln -s ~/dotfiles/.zshrc ~/.zshrc
+
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.gitignore ~/.gitignore
