7bc77d913852516604377074dc529518f64d6b6b
Henry Nelson <hcnelson99@gmail.com>
2016-05-26 11:25:51 -0400

    Remove fish greeting and run startx on login with fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,12 @@
 set fish_key_bindings fish_user_key_bindings
+
 set -x CVSROOT ":ext:henry@cvs.fsl.cs.sunysb.edu:/scm/cvsroot"
 set -x CVS_RSH "/usr/bin/ssh"
+
+set -e fish_greeting
+
+if status --is-login
+    if test -z "$DISPLAY" -a $XDG_VTNR -eq 1
+        exec startx -- -keeptty
+    end
+end
