ce4ff0d442b6c5cf48b5e5bef160734bfb54d80a
Henry Nelson <hcnelson99@gmail.com>
2017-01-25 19:43:04 -0500

    Adjust install script for neovim

--- install.sh
+++ install.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 set -ux
 
-mkdir -p ~/.vim/backup ~/.vim/swap ~/.vim/undo
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 stow vim
-nvim +PlugInstall +qall
+mkdir -p ~/.config
+ln -s ~/.vim ~/.config/nvim
+ln -s ~/.vimrc ~/.config/nvim/init.vim
 
 git clone https://github.com/tarjoilija/zgen.git ~/.zgen
 stow zsh
 
-git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
-stow spacemacs
+#git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+#stow spacemacs
 stow X
 stow fontconfig
 stow git
